946294f50da26811804980ef891cba9646cad63b
princewritecode <princewritecode@gmail.com>
2024-11-19 10:36:54 +0530

    Adding js resources odd even

---
+++ odd_even.js
@@ -0,0 +1,11 @@
+let inputNumber1 = Number(prompta("Enter a number"));
+let inputNumber2 = Number(prompt("Enter another number2"));
+
+if (inputNumber1 % 2 === 0)
+{
+    console.log('The number is even');
+}
+else
+{
+    console.log('The number is odd');
+}
