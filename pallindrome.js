3313067a743e0562b1f774b573572b84987fb12f
princewritecode <princewritecode@gmail.com>
2024-11-23 15:57:29 +0530

    code wars quesiton stringy string solved

--- pallindrome.js
+++ pallindrome.js
@@ -3,7 +3,6 @@
 function pallindrome(str)
 {
     const reverseStr = str.split("").reverse().join("");
-
     return (reverseStr === str) ? ('pallindrome') : ('not pallindrome');
 }
 console.log(pallindrome('moom'));
