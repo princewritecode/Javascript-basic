d20a6ab27a973ed4a113ac6e0c5c21d9081db17f
princewritecode <princewritecode@gmail.com>
2024-12-07 12:37:25 +0530

    exception handling in js

--- document_write.js
+++ document_write.js
@@ -1,22 +1,32 @@
-document.write('<h1>Hello world</h1>');
-for (let i = 1; i < 5; i++)
-{
-    for (let j = 1; j < i; j++)
-    {
-        document.write('*');
-    }
-    document.write('<br>');
-}
-const items = document.querySelectorAll('.item');
-console.log(items);
-for (let item of items)
-{
-    item.classList.add('red');
-}
-const goodGuy = document.querySelectorAll('[data-type="good-guy"]');
+// document.write('<h1>Hello world</h1>');
+// for (let i = 1; i < 5; i++)
+// {
+//     for (let j = 1; j < i; j++)
+//     {
+//         document.write('*');
+//     }
+//     document.write('<br>');
+// }
+// const items = document.querySelectorAll('.item');
+// console.log(items);
+// for (let item of items)
+// {
+//     item.classList.add('red');
+// }
+// const goodGuy = document.querySelectorAll('[data-type="good-guy"]');
 
-console.log(goodGuy);
-for (let guy of goodGuy)
-{
-    guy.classList.add('yay');
-}
+// console.log(goodGuy);
+// for (let guy of goodGuy)
+// {
+//     guy.classList.add('yay');
+// }
+
+// const badGuys = document.querySelectorAll('[data-type="villain"]');
+// console.log(badGuys);
+
+// for (let bad of badGuys)
+// {
+//     bad.classList.add('nay');
+// }
+
+
---
+++ exception.js
@@ -0,0 +1,9 @@
+// try
+// {
+//     console.log(hellosss);
+// }
+// catch (err)
+// {
+//     console.log(err, 'this is the exception thrown');
+// }
+
