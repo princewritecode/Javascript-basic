da86297622c8989c04e6070ea75d2d80beedc1fc
princewritecode <princewritecode@gmail.com>
2024-12-11 10:31:55 +0530

    exception handling in js

--- exception.js
+++ exception.js
@@ -7,3 +7,20 @@
 //     console.log(err, 'this is the exception thrown');
 // }
 
+//scope local and global scope
+
+// let a = 21;
+// let b = 22;
+// let c = 12;
+// function outsideAdd()
+// {
+//     let a = 11;
+//     let b = 11;
+//     function add()
+//     {
+//         console.log(a + b + c);
+//     }
+//     add();
+// }
+// outsideAdd();
+// document.body.childNodes[1].innerText = "you are not welcome";
